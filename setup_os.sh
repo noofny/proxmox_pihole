0575a7afcf8d3c15c937f33dc4463a133ae50695
noofny <noofny@gmail.com>
2021-11-07 20:09:25 +1100

    Improves on SSH/user access.

--- setup_os.sh
+++ setup_os.sh
@@ -41,12 +41,32 @@ apt-get install -y \
 # firewall
 echo "Enabling and configuring firewall..."
 ufw enable
+ufw allow 22
 ufw allow 53
 ufw allow 80
 ufw allow 443
 ufw allow http
 ufw allow https
 ufw status
+
+
+# ssh/user
+echo "Configuring SSH and user access..."
+SSH_USER=piholeadmin
+adduser --gecos "" ${SSH_USER}
+usermod -aG sudo ${SSH_USER}
+ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa_pihole_ssh -q -N ""
+cat ~/.ssh/id_rsa_pihole_ssh
+mkdir /home/${SSH_USER}/.ssh
+cp ~/.ssh/id_rsa_pihole_ssh /home/${SSH_USER}/.ssh/id_rsa_pihole_ssh
+cp ~/.ssh/id_rsa_pihole_ssh.pub /home/${SSH_USER}/.ssh/id_rsa_pihole_ssh.pub
+cat ~/.ssh/id_rsa_pihole_ssh.pub >> /home/${SSH_USER}/.ssh/authorized_keys
+chown -R ${SSH_USER}:${SSH_USER} /home/${SSH_USER}
+chmod 600 /home/${SSH_USER}/.ssh/id_rsa*
+chmod 600 /home/${SSH_USER}/.ssh/authorized_keys
+sed -i -e 's/#PermitRootLogin prohibit-password/PermitRootLogin no/' /etc/ssh/sshd_config
+sed -i -e 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+systemctl restart ssh
 
 
 echo "Setup OS : script complete!"
